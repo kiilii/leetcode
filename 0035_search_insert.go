75f06cbd0dc75a0cb59a66a86032cf5cb9dbe6bb
kiilii <tonight17@126.com>
2022-05-24 21:54:50 +0800

    fix: 0035_search_insert

--- 0035_search_insert.go
+++ 0035_search_insert.go
@@ -9,7 +9,7 @@ func Test_0035_search_insert() {
 	fmt.Println(searchInsert(nums, 1))
 }
 
-// 2023.04.30
+// 2023.05.22
 // 执行用时：0 ms, 在所有 Go 提交中击败了 100.00% 的用户
 // 内存消耗：2.8 MB, 在所有 Go 提交中击败了 57.56% 的用户
 func searchInsert(nums []int, target int) int {
