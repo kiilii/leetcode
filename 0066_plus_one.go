7c97929d848d6acd948a0237c8ba2bfe14731baa
kiilii <tonight17@126.com>
2022-05-29 21:09:25 +0800

    fix: 0066_plus_one

---
+++ 0066_plus_one.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var nums = []int{1, 2, 3, 4, 5}
+
+	var nums = []int{2, 9, 2, 9}
+
+	fmt.Println(nums)
+	fmt.Println(nums[0:])
+	fmt.Println(nums[1:])
+
+	fmt.Println(plusOne(nums))
+}
+
+// 2023.05.29
+// 执行用时：0 ms, 在所有 Go 提交中击败了 100.00% 的用户
+// 内存消耗：1.9 MB, 在所有 Go 提交中击败了 96.45% 的用户
+func plusOne(digits []int) []int {
+	var length = len(digits)
+	digits[length-1] += 1
+
+	for i := length - 1; i > 0; i-- {
+		if digits[i]/10 > 0 {
+			digits[i] = digits[i] % 10
+			digits[i-1] += 1
+		}
+	}
+
+	if digits[0] == 10 {
+		digits = append(digits, 0)
+		digits[0] = 0
+		for ; length > 0; length-- {
+			digits[length] = digits[length-1]
+		}
+		digits[0] = 1
+	}
+
+	return digits
+}
