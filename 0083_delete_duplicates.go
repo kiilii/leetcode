b6bcbe32e28ea9b6902ef6187964eec7eaad4926
kiilii <tonight17@126.com>
2022-07-21 22:48:42 +0800

    fix: 0083_delet_dupliecate

---
+++ 0083_delete_duplicates.go
@@ -0,0 +1,31 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+// 2023.07.21
+// 执行用时：0 ms, 在所有 Go 提交中击败了 100.00% 的用户
+// 内存消耗：3 MB, 在所有 Go 提交中击败了 59.81% 的用户
+func deleteDuplicates(head *ListNode) *ListNode {
+	var fast, slow = head, head
+
+	for slow != nil {
+		if fast.Next == nil {
+			slow.Next = fast.Next
+			break
+		} else {
+			fast = fast.Next
+			if slow.Val == fast.Val {
+				continue
+			}
+			slow.Next = fast
+			slow = slow.Next
+		}
+	}
+
+	return head
+}
