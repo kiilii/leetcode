d05803b49313810057a043abb6e7a3bd32d28e6d
kiilii <tonight17@126.com>
2022-07-27 23:46:34 +0800

    fix: 0101_is_same_tree

---
+++ 0100_is_same_tree.go
@@ -0,0 +1,27 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+// 执行用时：0 ms, 在所有 Go 提交中击败了 100.00% 的用户
+// 内存消耗：1.9 MB, 在所有 Go 提交中击败了 48.29% 的用户
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil || q == nil {
+		return p == q
+	}
+	if !isSameTree(p.Left, q.Left) {
+		return false
+	}
+	if p.Val != q.Val {
+		return false
+	}
+	if !isSameTree(p.Right, q.Right) {
+		return false
+	}
+	return true
+}
