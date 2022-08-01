2e79b26c1ac4612082c4cd0677aad2d7961585ec
kiilii <tonight17@126.com>
2022-08-01 20:21:05 +0800

    fix: 0069_my_sqrt

--- 0069_my_sqrt.go
+++ 0069_my_sqrt.go
@@ -1,20 +1,12 @@
 package main
 
-import "fmt"
-
-func main() {
-	fmt.Println(mySqrt(12890))
-}
-
+// 速算平方根
+// 执行用时：0 ms, 在所有 Go 提交中击败了 100% 的用户
+// 内存消耗：2 MB, 在所有 Go 提交中击败了 99.87% 的用户
 func mySqrt(x int) int {
-	if x <= 1 {
-		return x
-	}
-
 	var y = x
 	for y*y > x {
 		y = (y + x/y) / 2
 	}
-
 	return y
 }
