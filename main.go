8c597894058e3683e79b6babc020371a3c68eb7f
kiilii <tonight17@126.com>
2022-08-20 20:54:29 +0800

    add:MakeLIstNode func

--- main.go
+++ main.go
@@ -14,3 +14,26 @@ type TreeNode struct {
 	Left  *TreeNode
 	Right *TreeNode
 }
+
+func MakeListNode(nodes []int) *ListNode {
+	if len(nodes) == 0 {
+		return nil
+	}
+
+	var head, next *ListNode
+
+	for i := 0; i < len(nodes); i++ {
+		if i == 0 {
+			head = new(ListNode)
+			head.Val = nodes[i]
+			next = head
+		} else {
+			if next.Next == nil {
+				next.Next = new(ListNode)
+				next = next.Next
+				next.Val = nodes[i]
+			}
+		}
+	}
+	return head
+}
