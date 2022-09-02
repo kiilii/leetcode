8226d6e759662b624dd03cd655df8c37f8318f21
kiilii <tonight17@126.com>
2022-09-02 23:38:59 +0800

    add: quick sort in 0217

--- 0217_contains_duplicate.go
+++ 0217_contains_duplicate.go
@@ -6,13 +6,35 @@ func containsDuplicate(nums []int) bool {
 	if len(nums) <= 1 {
 		return false
 	}
-
+	qs(nums)
 	for i := 1; i < len(nums); i++ {
 		if nums[i] == nums[i-1] {
 			return true
 		}
 	}
 	return false
+}
+
+// quick sort
+func qs(nums []int) {
+	if len(nums) <= 1 {
+		return
+	}
+	mid, i := nums[0], 1
+	head, tail := 0, len(nums)-1
+	for head < tail {
+		if nums[i] > mid {
+			nums[i], nums[tail] = nums[tail], nums[i]
+			tail--
+		} else {
+			nums[i], nums[head] = nums[head], nums[i]
+			head++
+			i++
+		}
+	}
+	nums[head] = mid
+	qs(nums[:head])
+	qs(nums[head+1:])
 }
 
 // 执行用时：76 ms, 在所有 Go 提交中击败了 36.13% 的用户
